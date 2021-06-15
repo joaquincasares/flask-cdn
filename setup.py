1478d07ca4ed53579f96da01bacd19463d1dc93d
Joaquin Casares <joaquin.casares@gmail.com>
2021-06-14 21:32:45 -0500

    Bump for an official release with CDN_VERSION feature

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='Flask-CDN',
-    version='1.5.3',
+    version='1.5.4',
     url='https://libwilliam.github.io/flask-cdn/',
     license='MIT',
     author='William Fagan',
